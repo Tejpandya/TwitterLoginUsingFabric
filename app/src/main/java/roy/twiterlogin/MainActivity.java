3bc70a2445893ba8bfc67e0101b76f1e5eeb1f63
kundan <kundan@canbrand.com>
2015-11-13 00:52:07 +0530

    First commit

---
+++ app/src/main/java/roy/twiterlogin/MainActivity.java
@@ -0,0 +1,125 @@
+package roy.twiterlogin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.models.User;
+import com.twitter.sdk.android.core.services.StatusesService;
+import com.twitter.sdk.android.tweetcomposer.TweetComposer;
+
+import io.fabric.sdk.android.Fabric;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "R2b2zU3cvJY38PDrZEWrIesH7";
+    private static final String TWITTER_SECRET = "GmsfJH3kNYAWgGZ87qgUBkYbtE5ZL9osExRkvIbV6g3HBVC8iF";
+
+    Long userid;
+    TwitterSession session;
+
+    private TwitterLoginButton loginButton;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.tv_username);
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+
+                Log.d("Twitter ", "Login sucessfull");
+                session = result.data;
+
+                String username = session.getUserName();
+                userid = session.getUserId();
+
+
+                textView.setText("Hi " + username);
+                TwitterAuthToken authToken = session.getAuthToken();
+                String token = authToken.token;
+                String secret = authToken.secret;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+
+                /*TweetComposer.Builder builder = new TweetComposer.Builder(MainActivity.this).text("Just setting up my Fabric!");
+                builder.show();*/
+                getUserData();
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
+
+
+    }
+
+    void getUserData() {
+        Twitter.getApiClient(session).getAccountService()
+                .verifyCredentials(true, false, new Callback<User>() {
+
+                    @Override
+                    public void failure(TwitterException e) {
+
+                    }
+
+                    @Override
+                    public void success(Result<User> userResult) {
+
+                        User user = userResult.data;
+                        String twitterImage = user.profileImageUrl;
+
+                        try {
+                            Log.d("imageurl", user.profileImageUrl);
+                            Log.d("name", user.name);
+                            //Log.d("email",user.email);
+                            Log.d("des", user.description);
+                            Log.d("followers ", String.valueOf(user.followersCount));
+                            Log.d("createdAt", user.createdAt);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+
+
+                    }
+
+                });
+// Can also use Twitter directly: Twitter.getApiClient()
+
+    }
+}
